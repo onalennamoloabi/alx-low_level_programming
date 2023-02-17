d2d17fcfd618af01dfbb62ee6bf7fd4a1c5a50a5
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-16 17:20:13 -0800

    How big are the data types?

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the output is the sizes of all of the data types
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("char size(bytes): %zu\n", sizeof(char));
+	printf("short size(bytes): %zu\n", sizeof(short));
+	printf("int size(bytes): %zu\n", sizeof(int));
+	printf("long size(bytes): %zu\n", sizeof(long));
+	printf("long long size(bytes): %zu\n", sizeof(long long));
+	printf("float size(bytes): %zu\n", sizeof(float));
+	printf("double size(bytes): %zu\n", sizeof(double));
+	printf("long double size (bytes): %zu\n", sizeof(long double));
+	return (0);
+}
+
