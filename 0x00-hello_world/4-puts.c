8e4d432fa6df741353053b22db5f2c8078f88783
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-16 16:30:32 -0800

    Coding in C!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/*
+ * main - print a text using puts followed by a new line
+ * 
+ * return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
