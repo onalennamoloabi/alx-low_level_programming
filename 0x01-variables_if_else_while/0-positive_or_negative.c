363a532356cee5bfa3a7bfc4bbf80ecfe0812fd6
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-17 15:19:47 -0800

    Under the following conditions

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,14 +13,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		print("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	return (0);
 
 	else if (n == 0)
-		print("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	return (0);
 
 	else
-		print("%d is negative\n");
+		printf("%d is negative\n");
 	return (0);
 }
