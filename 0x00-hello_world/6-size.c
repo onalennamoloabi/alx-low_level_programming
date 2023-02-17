f338a4afa7830227e5b6f70b1f35d9578522d9fc
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-16 17:34:21 -0800

    It's got to be this one

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdint,h>
+#include <stdint.h>
 /**
  * main - the output is the sizes of all of the data types
  *
@@ -9,7 +9,8 @@ int main(void)
 {
 	printf("char size(bytes): %zu\n", sizeof(char));
 	printf("short size(bytes): %zu\n", sizeof(short));
-	printf("int size(bytes): %zu\n", sizeof(int));
+	printf("int32_t size(bytes): %zu\n", sizeof(int32_t));
+	printf("int64_t size(bytes): %zu\n", sizeof(int64_t));
 	printf("long size(bytes): %zu\n", sizeof(long));
 	printf("long long size(bytes): %zu\n", sizeof(long long));
 	printf("float size(bytes): %zu\n", sizeof(float));
