2e68a7bd4a46ddf354565e4512d37238ecfd8fce
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-17 18:13:29 -0800

    All but q and e ABC

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print the alphabet except q and e
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char alpha_lower, e, q;
+
+	for (alpha_lower = 'a'; alpha_lower <= 'z'; alpha_lower++)
+	{
+		if (alpha_lower != e && alpha_lower != q)
+			putchar(alpha_lower);
+	}
+	putchar('\n')
+		return (0)
+}
