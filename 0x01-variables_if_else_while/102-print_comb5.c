3d57f92f916be26ce4805fc5e8b75f8f77a9b1a1
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-25 15:36:59 -0800

    Now I can print all possible combos of 2-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,34 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
+
 /**
  * main - prints all possible combinations of two-digit numbers
  *
- * return: 0
+ *Return: 0(SUCCESS)
  */
-int main() 
+int main(void)
 {
 	int i, j;
-    for (i = 0; i < 100; i++) {
-        for (j = i; j < 100; j++) {
-            int a = i / 10;
-            int b = i % 10;
-            int c = j / 10;
-            int d = j % 10;
-            putchar(a + '0');
-            putchar(b + '0');
-            putchar(' ');
-            putchar(c + '0');
-            putchar(d + '0');
-            if (i != 99 || j != 99) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    return 0;
-}
 
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			int a = i / 10;
+			int b = i % 10;
+			int c = j / 10;
+			int d = j % 10;
+
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(' ');
+			putchar(c + '0');
+			putchar(d + '0');
+			if (i != 99 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
