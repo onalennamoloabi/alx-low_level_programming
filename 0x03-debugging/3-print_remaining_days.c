fa9a942b081be5c7fd8dbf445fa8763fc0f7394e
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-25 06:16:30 -0800

    How many days left in the year?

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -4,6 +4,7 @@
 /**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
+* day_of_year - calculates the day of the year
 * @month: month in number format
 * @day: day of month
 * @year: year
@@ -12,28 +13,44 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	int days_in_feb = 28;
+	int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int days_remaining = 0;
+	int i;
 
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 	{
-		days_in_feb = 29;
+		days_in_month[2] = 29;
 	}
-		if (month < 1 || month > 12)
+	for (i = month; i <= 12; i++)
+	{
+		if (i == month)
 		{
-			printf("Invalid date: month %d is out of range.\n" month);
-			return;
+			days_remaining += days_in_month[i] - day;
 		}
-		if (day < 1 || day > days_in_month[month - 1] ||
-				(month == 2 && day > days_in_feb))
+		else
 		{
-			printf("Invalid date: day %d is out of range for month %d\n", day, month);
-			return;
+			days_remaining += days_in_month[i];
 		}
-		if (month >= 2 && day >= 60 && days_in_feb == 28)
-		{
-			day++;
-		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", days_in_feb == 29 ? 366 - day : 365 - day);
+	}
+	printf("Day of the year: %d\n", day_of_year(month, day, year));
+	printf("Remaining days: %d\n", days_remaining);
 }
+
+int day_of_year(int month, int day, int year)
+{
+	int i;
+	int days = 0;
+	int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+	{
+		days_in_month[2] = 29;
+	}
+	for (i = 1; i < month; i++)
+	{
+		days += days_in_month[i];
+	}
+	days += day;
+	return (days);
+}
+
