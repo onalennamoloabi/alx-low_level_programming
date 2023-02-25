5c7826801bd1b9f7d93eb0490a68f878ee23d729
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-25 07:42:54 -0800

    Printing all single digits of base 10!

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints digits of base 10 from 0
+ * Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= 0; digit++)
+	{
+		putchar(digit);
+		putchar('\n');
+	}
+	return (0);
+}
