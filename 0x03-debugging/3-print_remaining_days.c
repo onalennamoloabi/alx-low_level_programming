9673adb13164bc421e1c4aa68abd9498b1be9f29
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-25 05:48:43 -0800

    How many days left?

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	int days_in_feb = 28;
-	
+
 	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 		days_in_feb = 29;
@@ -24,7 +24,8 @@ void print_remaining_days(int month, int day, int year)
 			printf("Invalid date: month %d is out of range.\n" month);
 			return;
 		}
-		if (day < 1 || day > days_in_month[month - 1] || (month == 2 && day > days_in_feb))
+		if (day < 1 || day > days_in_month[month - 1] ||
+				(month == 2 && day > days_in_feb))
 		{
 			printf("Invalid date: day %d is out of range for month %d\n", day, month);
 			return;
