17b986a15977adc261bcb371c8a43a4f0be72017
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-25 06:39:17 -0800

    Made printing numbers Betty compliant

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+/**
+ * main - prints numbers of base 10
+ *
+ * Return: 0 (SUCCESS)
+ */
 
-int main()
-{
-	in #include <stdio.h>
-
-int main()
+int main(void)
 {
 	int i;
-	for (i = 0; i < 10; i++) {
-        printf("%d\n", i);
-    }
-    return 0
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
 }
