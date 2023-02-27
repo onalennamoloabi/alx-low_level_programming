6d8b32c0875473c77eb53b445a705b155c0540b9
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-27 12:26:00 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the int value to 98 through its pointer
+ * @n: pointer to the int
+ * Return: new int value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
