24272b21f1fa8734e79eba483a8d0579154e4759
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-02-28 05:10:23 -0800

    4. I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to the string
+ * Return: 0(SUCCESS)
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This repository is for:
 - The C - Hello, World project
 - The C - Variables, if, else, while project
+- The C - More functions, more nested loops project
+- The C - Debugging project
