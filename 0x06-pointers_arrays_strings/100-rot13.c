4d69d47fc03d440c47f4c10231d629e545b3448f
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-03-02 17:31:42 -0800

    8. rot13 mod3

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,33 +13,22 @@ char *rot13(char *str)
 	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 	'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
 	'w', 'x', 'y', 'z'};
-	char keyrot13[52]
-
-		for (index2 = 0; index2 < 52; index2++)
-		{
-			if (index2 < 26)
-			{
-				key[index2] = alphabet[(index2 + n) % 26];
-			}
-			else
-			{
-				key[index2] = alphabet[26 + (index2 + n - 26) % 26];
-			}
-		}
+	char keyrot13[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+		'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D',
+		'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n',
+		'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm'};
 
 	while (str[index1])
 	{
-		if ((str[index1] >= 'A' && str[index1] <= 'Z') ||
-				(str[index1] >= 'a' && str[index1] <= 'z'))
+		for (index2 = 0; index2 < 52; index2++)
 		{
-			for (index2 = 0; index2 < 52; index2++)
-			{
-				if (str[index1] == alphabet[index2])
-				{
-					str[index1] = key[index2];
-					break;
-				}
-			}
+			 if (str[index1] == alphabet[index2])
+			 {
+				 str[index1] = keyrot13[index2];
+				 break;
+			 }
 		}
 		index1++;
 	}
