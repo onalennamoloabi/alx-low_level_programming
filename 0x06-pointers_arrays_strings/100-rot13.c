bb868f7227379547c9b13fc90dc8a994c4874114
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-03-02 17:24:17 -0800

    8. rot13 mod 2

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,12 +13,19 @@ char *rot13(char *str)
 	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 	'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
 	'w', 'x', 'y', 'z'};
-	char keyrot13[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-		'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D',
-		'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n',
-		'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-		'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-		'i', 'j', 'k', 'l', 'm'};
+	char keyrot13[52]
+
+		for (index2 = 0; index2 < 52; index2++)
+		{
+			if (index2 < 26)
+			{
+				key[index2] = alphabet[(index2 + n) % 26];
+			}
+			else
+			{
+				key[index2] = alphabet[26 + (index2 + n - 26) % 26];
+			}
+		}
 
 	while (str[index1])
 	{
@@ -29,7 +36,7 @@ char *rot13(char *str)
 			{
 				if (str[index1] == alphabet[index2])
 				{
-					str[index1] = keyrot13[index2];
+					str[index1] = key[index2];
 					break;
 				}
 			}
