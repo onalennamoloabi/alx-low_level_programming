5751e00c49eda4b2c0874b9c5ad10999da40578b
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-03-09 11:48:16 -0800

    Betty documentation error fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,8 @@
  * _pow_recursion - returns the value of x to the power y
  * @x: base number
  * @y: exponent
- * Returns: -1 if y<0
+ * Return: -1 if y < 0, 1 if y = 0 else, the value
+ * of x to the power of y
  */
 int _pow_recursion(int x, int y)
 {
