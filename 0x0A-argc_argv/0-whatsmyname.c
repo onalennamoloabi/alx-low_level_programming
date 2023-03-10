9c0868fe7796ad8da3d25a35874af354219c1050
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-03-10 11:56:47 -0800

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the program name
+ * @argc: number of arguments
+ * @argv: arguments counted
+ * Return: 0 (SUCCESS)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+In this project I learn of main prototypes, how to use arguments passed to the program and how to compile functions using __attribute__((unused)) or (void) with unused variables or parameters.
