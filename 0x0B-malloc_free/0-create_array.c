e087767d5e9819e5cd64254c5e4d5d2e4743d6c2
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-03-14 05:51:24 -0700

    Fixed some compiling errors and Betty requirements

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,21 +1,23 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 /**
- * create_array - creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array of chars, and
+ * initializes it with a specific char
  * @size: size of the array
  * @c: character to initialise array with
- * Returns: pointer to the array, NULL if size = 0, or NULL if it fails
+ * Return: pointer to the array, NULL if size = 0, or NULL if it fails
  */
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 
-	if (size = 0)
+	char* arr = (char*) malloc(size * sizeof(char));
+	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *arr = (char *)malloc(size * sizeof(char));
-	if (arr == NULL);
+
+	if (arr == NULL)
 	{
 		return (NULL);
 	}
