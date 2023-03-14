69da76c280490aca4a5d9a7589197a6b47b63428
Onalenna Moloabi <onalennamoloabi@gmail.com>
2023-03-14 05:57:17 -0700

    More errors fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,8 +10,8 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *arr = (char *) malloc(size * sizeof(char));
 
-	char* arr = (char*) malloc(size * sizeof(char));
 	if (size == 0)
 	{
 		return (NULL);
